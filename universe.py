c1549b9fa045aa724a30b5e5c234c4e299d7d6a1
Mathieu Bouchard <mathbouchard@gmail.com>
2013-02-21 16:41:31 -0500

    Create universe.py
    The Main classes for the tum project

---
+++ universe.py
@@ -0,0 +1,19 @@
+class Universe(object):
+    
+    def getElements(Filter)
+        ugetelements(Filter):
+
+class Element():
+    type "state"
+    def getInDualElements(Filter):
+        ugetindualelements(Object, Filter)
+    def getOutDualElements(Filter):
+        ugetoutdualelements(Object, Filter)
+    def getOutDualElements(Filter):
+        ugetoutdualelements(Object, Filter)
+    
+class Atributes:
+    name "default"
+    strval ""
+    dblval 0.0
+
